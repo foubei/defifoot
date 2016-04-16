4e82eeb31b31f317e13d0dd53b95a5134a963f3b
Tahir Fouzi <tahirfz-90@hotmail.fr>
2016-04-16 18:25:44 +0800

    Ajouter les couleurs communes dans un fichier.

--- Source/App/src/ReadPropertyFile.java
+++ Source/App/src/ReadPropertyFile.java
@@ -2,11 +2,11 @@ import java.io.IOException;
 import java.io.InputStream;
 
 /**
- * Get the data from the config.properties file.
+ * Get the data from the configuration files.
  */
 public class ReadPropertyFile {
-	public void main() {
-		InputStream input = ReadPropertyFile.class.getClassLoader().getResourceAsStream("config.properties");
+	public void main(String fileName) {
+		InputStream input = ReadPropertyFile.class.getClassLoader().getResourceAsStream(fileName);
 		try {
 			MainFrame.prop.load(input);
 		} catch (IOException e) {
